757b8f1f094cc4bee7db8ed0d2a938c1c53312b6
oleh.kurachenko <oleh.kurachenko@gmail.com>
2018-06-25 11:54:41 +0300

    inial commit (HT1 & HT2 already written)

---
+++ UnityProject-master/Assets/Content/Scripts/RabbitBehaviour.cs
@@ -0,0 +1,105 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RabbitBehaviour : MonoBehaviour
+{
+    public float speed = 1;
+    Rigidbody2D myBody = null;
+
+    bool isGrounded = false;
+    bool JumpActive = false;
+    float JumpTime = 0f;
+    public float MaxJumpTime = 2f;
+    public float JumpSpeed = 2f;
+
+    void Start()
+    {
+        this.myBody = this.GetComponent<Rigidbody2D>();
+        LevelController.current.setStartPosition(transform.position);
+    }
+
+    void Update()
+    {
+        //[-1, 1]
+        float value = Input.GetAxis("Horizontal");
+        if (Mathf.Abs(value) > 0)
+        {
+            Vector2 vel = myBody.velocity;
+            vel.x = value * speed;
+            myBody.velocity = vel;
+        }
+
+        Animator animator = GetComponent<Animator>();
+        if (Mathf.Abs(value) > 0)
+        {
+            animator.SetBool("run", true);
+        }
+        else
+        {
+            animator.SetBool("run", false);
+        }
+
+        SpriteRenderer sr = GetComponent<SpriteRenderer>();
+        if (value < 0)
+        {
+            sr.flipX = true;
+        }
+        else if (value > 0)
+        {
+            sr.flipX = false;
+        }
+
+        if (this.isGrounded)
+        {
+            animator.SetBool("jump", false);
+        }
+        else
+        {
+            animator.SetBool("jump", true);
+        }
+    }
+
+    void FixedUpdate()
+    {
+        Vector3 from = transform.position + Vector3.up * 0.3f;
+        Vector3 to = transform.position + Vector3.down * 0.1f;
+        int layer_id = 1 << LayerMask.NameToLayer("Ground");
+        //Перевіряємо чи проходить лінія через Collider з шаром Ground
+        RaycastHit2D hit = Physics2D.Linecast(from, to, layer_id);
+        if (hit)
+        {
+            isGrounded = true;
+        }
+        else
+        {
+            isGrounded = false;
+        }
+        //Намалювати лінію (для розробника)
+        Debug.DrawLine(from, to, Color.red);
+
+        if (Input.GetButtonDown("Jump") && isGrounded)
+        {
+            this.JumpActive = true;
+        }
+        if (this.JumpActive)
+        {
+            //Якщо кнопку ще тримають
+            if (Input.GetButton("Jump"))
+            {
+                this.JumpTime += Time.deltaTime;
+                if (this.JumpTime < this.MaxJumpTime)
+                {
+                    Vector2 vel = myBody.velocity;
+                    vel.y = JumpSpeed * (1.0f - JumpTime / MaxJumpTime);
+                    myBody.velocity = vel;
+                }
+            }
+            else
+            {
+                this.JumpActive = false;
+                this.JumpTime = 0;
+            }
+        }
+    }
+}
